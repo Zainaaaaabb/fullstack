84df30df991879790ed7464276c42504b0460829
zainabahmad <zainaa1121@gmail.com>
2023-05-04 23:18:46 +0300

    Part 2.11-2.17 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+//import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/person'
+import Notification from './components/Notification'
 
 
 // a function
@@ -12,30 +15,80 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setFilterQuery] = useState('')
+  const [status, setStatus] = useState(null)
+  const [message, setMessage] = useState(null)
+
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => setPersons(initialPersons))
+  }, [])
 
   const handleChange = setValue => (event) => setValue(event.target.value)
 
   const handleAddNewPerson = (event) => {
     event.preventDefault()
 
+    const newPerson = { name: newName, number: newNumber }
     // se eif person is already added to phonebook 
-    if (persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    const foundPerson = persons.find(person => person.name === newName)
+    if (foundPerson) {
+      // alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace?`)) {
+        personService.update(foundPerson.id, newPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== foundPerson.id ? person : returnedPerson))
+          }).catch(error => {
+            setStatus('error')
+            setMessage(
+              `Info of ${foundPerson.name} has already been removed`
+            )
+            setTimeout(() => {
+              setStatus(null)
+              setMessage(null)
+            }, 5000)
+
+            setPersons(persons.filter(person => person.id !== foundPerson.id))
+          })
+      }
     }
 
     else {
-      const newPerson = { name: newName, number: newNumber }
-      // concatenate new person to the persons list 
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      personService
+        .create(newPerson)
+        .then(addedPerson => {
+          // concatenate new person to the persons list 
+          setPersons(persons.concat(addedPerson))
+          setStatus('success')
+          setMessage(
+            `Added ${addedPerson.name}`
+          )
+          setTimeout(() => {
+            setStatus(null)
+            setMessage(null)
+          }, 5000)
+          setNewName('')
+          setNewNumber('')
 
+        })
+    }
+  }
+
+
+  const handleRemovePerson = (id, name) => () => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService.remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.name !== name))
+        })
     }
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={message} status={status} />
       <Filter
         query={filterQuery}
         handleChange={handleChange(setFilterQuery)} />
@@ -50,7 +103,9 @@ const App = () => {
       <h3>Numbers</h3>
       <Persons
         persons={persons}
-        query={filterQuery} />
+        query={filterQuery}
+        handleRemovePerson={handleRemovePerson}
+      />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,13 @@
-const Persons = ({ persons, query }) => {
+const Persons = ({ persons, query, handleRemovePerson }) => {
     return (
         <>
-            {persons.filter(person => person.name.toLowerCase().includes(query))
-                .map(person => (
+            {persons
+                .filter(person => person.name.toLowerCase().includes(query))
+                .map((person, id) => (
                     <div key={person.name} >
                         {person.name}
                         {person.number}
+                        <button onClick={handleRemovePerson(id, person.name)}>Delete</button>
                     </div>
                 ))}
         </>
